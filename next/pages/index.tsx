46304d684f92ac839d57598a268e99f1e20e9c72
joziasmartini <jozias.martini@gmail.com>
2021-04-25 18:37:03 -0300

    Change project to next

---
+++ next/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div classNameName={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main classNameName={styles.app}>
+        
+      </main>
+    </div>
+  )
+}
